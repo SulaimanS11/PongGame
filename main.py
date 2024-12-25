2acab5cc0c0e60161fae90dbc40561552b61b447
SulaimanS <95322305+SulaimanS11@users.noreply.github.com>
2024-12-25 16:29:28 -0500

    screen and paddle basic animations

---
+++ main.py
@@ -0,0 +1,48 @@
+﻿import turtle
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.bgcolor("lightgrey")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+
+paddle.penup()
+paddle.goto(-350,0)
+
+paddle.color("black")
+
+def paddle_up():
+    y = paddle.ycor()
+    y += 20
+    paddle.sety(y)
+
+def paddle_down():
+    y = paddle.ycor()
+    y -= 20
+    paddle.sety(y)
+
+
+game_on = True
+
+screen.onkey(paddle_up, "w")
+screen.onkey(paddle_down, "s")
+screen.listen()
+
+
+while game_on:
+    
+
+
+    screen.update()
+
+
+
+
+
+screen.exitonclick()
+
